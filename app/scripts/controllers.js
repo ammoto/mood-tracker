2b2034cb400cccde3574e792530516eca73bbe4a
muchimoto <muchimoto@gmail.com>
2015-01-24 17:04:06 -0500

    got firebase data

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,74 +1,113 @@
 'use strict';
 angular.module('Moodtracker.controllers', [])
+    // DASH
+    .controller('DashCtrl', function($scope, $firebase) {
 
-.controller('DashCtrl', function($scope,  $firebase) {
+        $scope.first = {
+            text: null
+        };
+        $scope.last = {
+            text: null
+        };
+        $scope.email = {
+            text: null
+        };
 
-  $scope.first = {text: null};
-  $scope.last = {text:null};
-  $scope.email ={text:null};
+        // $scope.moods = $firebase(new Firebase("https://mood-track.firebaseio.com/Moods"));
 
-  // $scope.moods = $firebase(new Firebase("https://mood-track.firebaseio.com/Moods"));
+        $scope.signUp = function() {
 
-  $scope.signUp = function() {
+            if ($scope.first.text === '') {
+                return;
+            }
+            if ($scope.last.text === '') {
+                return;
+            }
+            if ($scope.email.text === '') {
+                return;
+            }
 
-        if ($scope.name.text === '') {
-            return;
+            $scope.moods.$push({
+                name: $scope.name.text,
+                scale: $scope.scale.num,
+                comment: $scope.comment.text
+            });
+
         }
-         if ($scope.last.text === '') {
-            return;
+
+    })
+    // MOOD ENTRY
+    .controller('MoodEntryCtrl', function($scope, $firebase) {
+
+        $scope.name = {};
+        $scope.scale = {};
+        $scope.comment = {};
+        $scope.date = {};
+        $scope.date.today = new Date().toString();
+
+        $scope.saveMood = function() {
+
+
+            if ($scope.name.text === '') {
+                return;
+            }
+            $scope.moods.$push({
+                name: $scope.name.text,
+                scale: $scope.scale.num,
+                comment: $scope.comment.text,
+                date: $scope.date.today
+            });
         }
-        if ($scope.email.text === '') {
-            return;
-        }
-
-    $scope.moods.$push({name: $scope.name.text, scale: $scope.scale.num, comment: $scope.comment.text});
-
-  }
-
-})
-
-.controller('MoodEntryCtrl', function($scope, $firebase) {
-
-$scope.date = new Date();
+    })
 
 
-  $scope.moods = $firebase(new Firebase("https://mood-track.firebaseio.com/Moods"));
-
-  $scope.saveMood = function() {
-
-        if ($scope.name.text === '') {
-            return;
-        }
-    $scope.moods.$push({name: $scope.name.text, scale: $scope.scale.num, comment: $scope.comment.text, date:$scope.date});
-
-  }
-})
-
+// DATA CHARTS
 .controller('DataCtrl', function($scope, $firebase) {
 
-   $scope.greeting = "Resize the page to see the re-rendering";
-    $scope.data = [
-      {name: "Greg", score: 98},
-      {name: "Ari", score: 96},
-      {name: 'Q', score: 75},
-      {name: "Loser", score: 48}
-    ];
+    var sync = $firebase(new Firebase("https://mood-track.firebaseio.com/Moods"));
 
-  // $scope.name = {text: null};
-  // $scope.scale = {num:0};
-  // $scope.comment ={text:null};
-  // $scope.currentDate =  new time.Date();
+    $scope.data = sync.$asObject()
 
-  // $scope.moods = $firebase(new Firebase("https://mood-track.firebaseio.com/Moods"));
+    $scope.greeting = "Resize the page to see the re-rendering";
 
-  // $scope.saveMood = function() {
+    $scope.data = [{
+        name: "Greg",
+        score: 98
+    }, {
+        name: "Ari",
+        score: 96
+    }, {
+        name: 'Q',
+        score: 75
+    }, {
+        name: "Loser",
+        score: 48
+    }];
 
-  //       if ($scope.name.text === '') {
-  //           return;
-  //       }
-  //   $scope.moods.$push({name: $scope.name.text, scale: $scope.scale.num, comment: $scope.comment.text});
+    $scope.onClick = function(item) {
+        alert('clicked!');
+        $scope.$apply(function() {
+            if (!$scope.showDetailPanel)
+                $scope.showDetailPanel = true;
+            $scope.detailItem = item;
+        });
+    };
 
-  // }
+    // $scope.name = {text: null};
+    // $scope.scale = {num:0};
+    // $scope.comment ={text:null};
+    // $scope.currentDate =  new time.Date();
+
+    // $scope.moods = $firebase(new Firebase("https://mood-track.firebaseio.com/Moods"));
+
+    // $scope.saveMood = function() {
+
+    //       if ($scope.name.text === '') {
+    //           return;
+    //       }
+    //   $scope.moods.$push({name: $scope.name.text, scale: $scope.scale.num, comment: $scope.comment.text});
+
+    // }
 
 
 
@@ -76,7 +115,7 @@ $scope.date = new Date();
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+    $scope.friend = Friends.get($stateParams.friendId);
 })
 
 .controller('AccountCtrl', function($scope) {
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -26,4 +26,17 @@ angular.module('Moodtracker.services', [])
   };
 })
 
+// .factory("ItemModel", function($rootScope,$firebase) {
 
+// var refMoods = new Firebase("https://mood-track.firebaseio.com/Moods");
+// var scope = $rootScope.$new();
+// scope.moods = [];
+// $firebase(refMoods,scope,'moods');
+// return scope;
+
+//   // var scope = $rootScope.$new(),
+//   //     url = 'https://mood-track.firebaseio.com/Moods';
+//   // scope.items = [];
+//   // var promise = $firebase(new Firebase(url).child('Moods'), scope, 'items');
+//   // return scope;
+// })
