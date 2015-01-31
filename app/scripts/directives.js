848e130407fd26c3d6b9902cdbbe81e805c7a6d8
muchimoto <muchimoto@gmail.com>
2015-01-30 19:49:00 -0500

    began work on line chart directive

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,7 +1,8 @@
 'use strict';
 angular.module('Moodtracker.directives', ['d3'])
+
     //Today's Data bar chart directive
-      .directive('d3Bars', ['$window', '$timeout', 'd3Service',
+    .directive('d3Bars', ['$window', '$timeout', 'd3Service',
         function($window, $timeout, d3Service) {
             return {
                 restrict: 'A',
@@ -11,8 +12,6 @@ angular.module('Moodtracker.directives', ['d3'])
                     onClick: '&'
                 },
                 link: function(scope, ele, attrs) {
-
-
 
                     d3Service.d3().then(function(d3) {
 
@@ -106,20 +105,133 @@ angular.module('Moodtracker.directives', ['d3'])
         }
     ])
 
+.directive('d3Line', ['$window', '$timeout', 'd3Service',
+    function($window, $timeout, d3Service) {
+        return {
+            restrict: 'A',
+            scope: {
+                data: '=',
+                label: '@',
+                onClick: '&'
+            },
+            link: function(scope, ele, attrs) {
+
+        d3Service.d3().then(function(d3) {
+
+                    var renderTimeout;
+
+                    var svg = d3.select(ele[0])
+                        .append('svg')
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                    $window.onresize = function() {
+                        scope.$apply();
+                    };
+
+                    scope.$watch(function() {
+                        return angular.element($window)[0].innerWidth;
+                    }, function() {
+                        scope.render(scope.data);
+                    });
+
+                    // watch for data changes and re-render
+                    scope.$watch('data', function(newVals, oldVals) {
+                        return scope.render(newVals);
+                    }, true);
+
+                    scope.render = function(data) {
+                        svg.selectAll('*').remove();
+
+                        if (!data) return;
+                        if (renderTimeout) clearTimeout(renderTimeout);
+                        var data = data;
+                        renderTimeout = $timeout(function() {
+
+                            // Beginning of pasted code
+
+
+                            var margin = {
+                                    top: 20,
+                                    right: 20,
+                                    bottom: 30,
+                                    left: 50
+                                },
+                                width = 960 - margin.left - margin.right,
+                                height = 500 - margin.top - margin.bottom;
+
+                            var parseDate = d3.time.format("%d-%b-%y").parse;
+
+                            var x = d3.time.scale()
+                                .range([0, width]);
+                            var y = d3.scale.linear()
+                                .range([height, 0]);
+                            var xAxis = d3.svg.axis()
+                                .scale(x)
+                                .orient("bottom");
+                            var yAxis = d3.svg.axis()
+                                .scale(y)
+                                .orient("left");
+                            var line = d3.svg.line()
+                                .x(function(d) {
+                                    return x(d.date);
+                                })
+                                .y(function(d) {
+                                    return y(d.score);
+                                });
+                                var filter = function(data) {
+                                data.forEach(function(d) {
+                                    d.date = parseDate(d.date);
+                                    d.score = +d.score;})};
+                                filter();
+                                x.domain(d3.extent(data, function(d) {
+                                    return d.date;
+                                }));
+                                y.domain(d3.extent(data, function(d) {
+                                    return d.score;}));
+                                svg.append("g")
+                                    .attr("class", "x axis")
+                                    .attr("transform", "translate(0," + height + ")")
+                                    .call(xAxis);
+                                svg.append("g")
+                                    .attr("class", "y axis")
+                                    .call(yAxis)
+                                    .append("text")
+                                    .attr("transform", "rotate(-90)")
+                                    .attr("y", 6)
+                                    .attr("dy", ".71em")
+                                    .style("text-anchor", "end")
+                                    .text("Price ($)");
+                                svg.append("path")
+                                    .datum(data)
+                                    .attr("class", "line")
+                                    .attr("d", line);
+                         //END OF RENDER TIMEOUT
+                         }, 200);
+                        }
+                    });
+                }
+            }
+        }
+    ])
+
+
 
 .directive('twitter', [
     function() {
         return {
             link: function(scope, element, attr) {
                 setTimeout(function() {
-                        twttr.widgetscreateShareButton(
-                            attr.url,
-                            element[0],
-                            function(el) {}, {
-                                count: 'none',
-                                text: attr.text
-                            }
-                        );
+                    twttr.widgetscreateShareButton(
+                        attr.url,
+                        element[0],
+                        function(el) {}, {
+                            count: 'none',
+                            text: attr.text
+                        }
+                    );
                 });
             }
         }
