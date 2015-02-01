311e2a0597f067357d2ea65c6f1a6cf5f5f30a35
muchimoto <muchimoto@gmail.com>
2015-01-31 23:56:40 -0500

    new chart

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -105,124 +105,133 @@ angular.module('Moodtracker.directives', ['d3'])
     }
 ])
 
-.directive('d3Line', ['$window', '$timeout', 'd3Service',
-            function($window, $timeout, d3Service) {
-                return {
-                    restrict: 'A',
-                    scope: {
-                        data: '=',
-                        label: '@',
-                        onClick: '&'
-                    },
-                    link: function(scope, ele, attrs) {
+// .directive('d3Line', ['$window', '$timeout', 'd3Service',
+//             function($window, $timeout, d3Service) {
+//                 return {
+//                     restrict: 'A',
+//                     scope: {
+//                         data: '=',
+//                         label: '@',
+//                         onClick: '&'
+//                     },
+//                     link: function(scope, ele, attrs) {
 
-                        d3Service.d3().then(function(d3) {
+//                         d3Service.d3().then(function(d3) {
 
-                                var renderTimeout;
+//                                 var renderTimeout;
 
-                                var margin = {
-                                        top: 20,
-                                        right: 20,
-                                        bottom: 30,
-                                        left: 50
-                                    },
-                                    width = 960 - margin.left - margin.right,
-                                    height = 500 - margin.top - margin.bottom;
+//                                 var margin = {
+//                                         top: 20,
+//                                         right: 20,
+//                                         bottom: 30,
+//                                         left: 50
+//                                     },
+//                                     width = 960 - margin.left - margin.right,
+//                                     height = 500 - margin.top - margin.bottom;
 
-                                var svg = d3.select(ele[0])
-                                    .append('svg')
-                                    .attr("width", width + margin.left + margin.right)
-                                    .attr("height", height + margin.top + margin.bottom)
-                                    .append("g")
-                                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//                                 var svg = d3.select(ele[0])
+//                                     .append('svg')
+//                                     .attr("width", width + margin.left + margin.right)
+//                                     .attr("height", height + margin.top + margin.bottom)
+//                                     .append("g")
+//                                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-                                $window.onresize = function() {
-                                    scope.$apply();
-                                };
+//                                 $window.onresize = function() {
+//                                     scope.$apply();
+//                                 };
 
-                                var parseDate = d3.time.format("%d-%b-%y").parse;
+                               
 
+//                                        scope.data.forEach(function(d) {
+//                                                     d.date = d3.time.format("%d-%B-%y").parse(d.date);
+//                                                     d.scale = +d.scale;
 
-                                
-
-                                scope.$watch(function() {
-                                    return angular.element($window)[0].innerWidth;
-                                }, function() {
-
-                                    scope.render(scope.data);
-                                });
-
-                                // watch for data changes and re-render
-                                scope.$watch('data', function(newVals, oldVals) {
-                                   
-                                    return scope.render(newVals);
-                                
-                                }, true);
-
-                                    scope.render = function(data) {
-                                        svg.selectAll('*').remove();
-
-                                        if (!data) return;
-                                        if (renderTimeout) clearTimeout(renderTimeout);
-
-                                        renderTimeout = $timeout(function() {
-
-                                            // Beginning of pasted code
-                                          
-
-                                            var x = d3.time.scale()
-                                                .range([0, width]);
-                                            var y = d3.scale.linear()
-                                                .range([height, 0]);
-                                            var xAxis = d3.svg.axis()
-                                                .scale(x)
-                                                .orient("bottom");
-                                            var yAxis = d3.svg.axis()
-                                                .scale(y)
-                                                .orient("left");
-                                            var line = d3.svg.line()
-                                                .x(function(d) {
-                                                    return x(d.date);
-                                                })
-                                                .y(function(d) {
-                                                    return y(d.scale);
-                                                });
-
-                                            x.domain(d3.extent(data, function(d) {
-                                                return d.date;
-                                            }));
-                                            y.domain(d3.extent(data, function(d) {
-                                                return d.scale;
-                                            }));
-                                            svg.append("g")
-                                                .attr("class", "x axis")
-                                                .attr("transform", "translate(0," + height + ")")
-                                                .call(xAxis)
+//                                                 });
 
 
-                                            svg.append("g")
-                                                .attr("class", "y axis")
-                                                .call(yAxis)
-                                                .append("text")
-                                                .attr("transform", "rotate(-90)")
-                                                .attr("y", 6)
-                                                .attr("dy", ".71em")
-                                                .style("text-anchor", "end")
-                                                .text("Scale");
+//                                 scope.$watch(function() {
+//                                     return angular.element($window)[0].innerWidth;
+//                                 }, function() {
 
-                                            svg.append("path")
-                                                .datum(data)
-                                                .attr("class", "line")
-                                                .attr("d", line());
+//                                     scope.render(scope.data);
+//                                 });
 
-                                            //END OF RENDER TIMEOUT
-                                        }, 200);
-                                    }
-                                });
-                            }
-                        }
-                    }
-                ])
+//                                 // watch for data changes and re-render
+//                                 scope.$watch('data', function(newVals, oldVals) {
+
+//                                     return scope.render(newVals);
+
+//                                 }, true);
+
+//                                 scope.render = function(data) {
+//                                     svg.selectAll('*').remove();
+
+//                                     if (!data) return;
+//                                     if (renderTimeout) clearTimeout(renderTimeout);
+
+//                                     renderTimeout = $timeout(function() {
+
+//                                             // Beginning of pasted code
+
+
+//                                             var x = d3.time.scale()
+//                                                 .range([0, width]);
+//                                             var y = d3.scale.linear()
+//                                                 .range([height, 0]);
+//                                             var xAxis = d3.svg.axis()
+//                                                 .scale(x)
+//                                                 .orient("bottom");
+//                                             var yAxis = d3.svg.axis()
+//                                                 .scale(y)
+//                                                 .orient("left");
+//                                             var line = d3.svg.line()
+//                                                 .x(function(d) {
+//                                                     return x(d.date);
+//                                                 })
+//                                                 .y(function(d) {
+//                                                     return y(d.scale);
+//                                                 });
+
+                                    
+//                                                 x.domain(d3.extent(data, function(d) {
+//                                                     return d.date;
+//                                                 }));
+//                                                 y.domain(d3.extent(data, function(d) {
+//                                                     return d.scale;
+//                                                 }));
+
+//                                                  // svg.append("path")
+//                                                  //    .datum(data)
+//                                                  //    .attr("class", "line")
+//                                                  //    .attr("d", line(data));
+
+//                                                 svg.append("g")
+//                                                     .attr("class", "x axis")
+//                                                     .attr("transform", "translate(0," + height + ")")
+//                                                     .call(xAxis)
+
+
+//                                                 svg.append("g")
+//                                                     .attr("class", "y axis")
+//                                                     .call(yAxis)
+//                                                     .append("text")
+//                                                     .attr("transform", "rotate(-90)")
+//                                                     .attr("y", 6)
+//                                                     .attr("dy", ".71em")
+//                                                     .style("text-anchor", "end")
+//                                                     .text("Scale");
+
+                                               
+                                             
+
+//                                                 //END OF RENDER TIMEOUT
+//                                             }, 200);
+//                                         }
+//                                     });
+//                             }
+//                         }
+//                     }
+//                 ])
 
 
 
